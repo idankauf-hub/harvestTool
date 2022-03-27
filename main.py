824a882e5f4d470361e4d6fa12bf57e86a62922b
idankauf <idankauf@gmail.com>
2022-03-27 16:05:44 +0300

    finish

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# This is a sample Python script.
 
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
 import json
 
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    os.system(r'cd C:\Projects\theHarvester && theHarvester.py -d bing.com -b bing -f "data"')
     with open(r"C:\Projects\theHarvester\data.json") as f:
         data = json.load(f)
+
     output = []
 
     for element in data["hosts"]:
@@ -13,8 +19,7 @@ if __name__ == '__main__':
             output.append({"asset": line[0], "ip": line[1]})
         else:
             output.append({"asset": element, "ip": "No ip"})
-    print(output)
+    print("JSON File: " + output)
 
     with open(r"C:\Projects\theHarvester\data.json", 'w') as json_file:
         json.dump(output, json_file)
-
